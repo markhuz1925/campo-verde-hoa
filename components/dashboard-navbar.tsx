7a63e40931c4781f6d144e274dd44bc0c0447329
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-12-10 23:00:52 +0800

    Update DashboardNavbar component.

--- components/dashboard-navbar.tsx
+++ components/dashboard-navbar.tsx
@@ -40,7 +40,12 @@ export function DashboardNavbar() {
   ];
 
   return (
-    <nav className="fixed z-50 top-0 px-4 py-3 w-full h-fit border-b shadow-sm bg-white flex items-center justify-between">
+    <nav
+      className={cn(
+        "fixed z-50 top-0 px-4 py-3 w-full h-fit border-b shadow-sm bg-white flex items-center justify-between",
+        pathname === "/sign-in" ? "hidden opacity-0" : ""
+      )}
+    >
       <div className="flex items-center gap-4">
         <Image
           src="/logo.svg"
