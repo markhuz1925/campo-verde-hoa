933a0d3e75047da824c2d24e5e5e48dff62829f3
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-12-10 21:02:33 +0800

    Update Sticker page.

--- app/(platform)/(dashboard)/sticker/page.tsx
+++ app/(platform)/(dashboard)/sticker/page.tsx
@@ -1,3 +1,13 @@
-export default function StickerPage() {
-  return <div>Sticker</div>;
+import prisma from "@/prisma/client";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+export default async function StickerPage() {
+  const { userId } = auth();
+
+  if (!userId) redirect("/sign-in");
+
+  const stickers = await prisma.sticker.findMany();
+
+  return <div className="pt-20 px-4"></div>;
 }
