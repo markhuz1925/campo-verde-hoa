b4fb1b0a094ed78f47b82e96476ef0a415f9542e
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-12-10 23:00:20 +0800

    Remove checking, it's already handled by the middleware.

--- app/(platform)/(dashboard)/sticker/page.tsx
+++ app/(platform)/(dashboard)/sticker/page.tsx
@@ -1,12 +1,6 @@
 import prisma from "@/prisma/client";
-import { auth } from "@clerk/nextjs";
-import { redirect } from "next/navigation";
 
 export default async function StickerPage() {
-  const { userId } = auth();
-
-  if (!userId) redirect("/sign-in");
-
   const stickers = await prisma.sticker.findMany();
 
   return <div className="pt-20 px-4"></div>;
