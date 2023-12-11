d1b1c1e91227ee71e68bb92cc13c3b8a594555c8
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-12-11 19:15:49 +0800

    Create stickers api endpoint.

---
+++ app/api/stickers/route.ts
@@ -0,0 +1,30 @@
+import prisma from "@/prisma/client";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { name, color, price } = body;
+
+    if (!userId) return new NextResponse("Unauthorized", { status: 401 });
+    if (!name) return new NextResponse("Name is required", { status: 400 });
+    if (!color) return new NextResponse("Color is required", { status: 400 });
+    if (!price) return new NextResponse("Price is required", { status: 400 });
+
+    const stickerPrice = await prisma.stickerPrice.create({
+      data: {
+        name,
+        color,
+        price,
+        userId,
+      },
+    });
+
+    return NextResponse.json(stickerPrice);
+  } catch (error) {
+    console.log("[STICKER_PRICE_POST]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
