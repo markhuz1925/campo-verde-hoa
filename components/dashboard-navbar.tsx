51b78e49ae8952d84c4c98ce0de90f9bc9895a94
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-12-11 22:04:00 +0800

    Add additional condition.

--- components/dashboard-navbar.tsx
+++ components/dashboard-navbar.tsx
@@ -43,7 +43,9 @@ export function DashboardNavbar() {
     <nav
       className={cn(
         "fixed z-50 top-0 px-4 py-3 w-full h-fit border-b shadow-sm bg-white flex items-center justify-between",
-        pathname === "/sign-in" ? "hidden opacity-0" : ""
+        pathname === "/sign-in" || pathname === "/sign-in/factor-one"
+          ? "hidden opacity-0"
+          : ""
       )}
     >
       <div className="flex items-center gap-4">
