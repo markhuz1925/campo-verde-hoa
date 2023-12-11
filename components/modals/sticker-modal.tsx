f63489dbfa9452f3cc04832901490a2a33525add
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-12-11 19:17:49 +0800

    Update StickerModal component.

--- components/modals/sticker-modal.tsx
+++ components/modals/sticker-modal.tsx
@@ -1,15 +1,72 @@
 "use client";
 
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { Modal } from "@/components/ui/modal";
 import { useModal } from "@/hooks/use-modal";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
 import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "../ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import toast from "react-hot-toast";
+
+const formSchema = z.object({
+  name: z.string().min(1, { message: "Name is required" }),
+  color: z.string().min(1, { message: "Color is required" }),
+  price: z.coerce.number().min(1),
+});
 
 export function StickerModal() {
   const [isMounted, setIsMounted] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const onOpen = useModal((state) => state.onOpen);
   const onClose = useModal((state) => state.onClose);
   const isOpen = useModal((state) => state.isOpen);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      color: "",
+      price: 0,
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setLoading(true);
+      await axios.post("/api/stickers", values);
+      await axios.post("/api/logs", {
+        title: `[STICKER_POST_SUCCESS] ${values.name}/${values.color}/${values.price}`,
+      });
+      toast.success(
+        `Sticker ${values.name}-${values.color}-${values.price} created`
+      );
+      onClose();
+    } catch (error) {
+      toast.error("Something went wrong");
+      await axios.post("/api/logs", { title: `[STICKER_POST_ERROR] ${error}` });
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
     setIsMounted(true);
@@ -26,11 +83,104 @@ export function StickerModal() {
   return (
     <Modal
       title="Setup Sticker"
-      description="Setup your sticker prices and colors"
+      description="You don't have any sticker yet, please add atleast one. You can add more in Settings page."
       isOpen={isOpen}
       onClose={onClose}
     >
-      sticker form
+      <div className="space-y-4 py-2 pb-4">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col gap-5"
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Sticker Name</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger disabled={loading}>
+                        <SelectValue placeholder="Select sticker name" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="ho">Home Owner</SelectItem>
+                      <SelectItem value="tenant">Tenant</SelectItem>
+                      <SelectItem value="visitor">Visitor</SelectItem>
+                      <SelectItem value="delivery">Delivery</SelectItem>
+                      <SelectItem value="special">Special Entity</SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="color"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Color</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger disabled={loading}>
+                        <SelectValue placeholder="Select sticker color" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="GREEN">GREEN</SelectItem>
+                      <SelectItem value="YELLOW">YELLOW</SelectItem>
+                      <SelectItem value="WHITE">WHITE</SelectItem>
+                      <SelectItem value="RED">RED</SelectItem>
+                      <SelectItem value="SILVER">SILVER</SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="price"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Price</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="number"
+                      disabled={loading}
+                      placeholder="9.99"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="pt-6 space-x-2 flex items-center justify-end">
+              <Button
+                onClick={onClose}
+                disabled={loading}
+                variant="secondary"
+                size="sm"
+              >
+                Cancel
+              </Button>
+              <Button type="submit" disabled={loading} size="sm">
+                Continue
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
     </Modal>
   );
 }
