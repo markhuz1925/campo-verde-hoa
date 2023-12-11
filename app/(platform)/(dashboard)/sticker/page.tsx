0e03b951dc2657160e38110532f60a7aa4eca549
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-12-11 12:35:13 +0800

    Render StickerModal component when sticker table is empty.

--- app/(platform)/(dashboard)/sticker/page.tsx
+++ app/(platform)/(dashboard)/sticker/page.tsx
@@ -1,7 +1,12 @@
+import { StickerModal } from "@/components/modals/sticker-modal";
 import prisma from "@/prisma/client";
 
 export default async function StickerPage() {
   const stickers = await prisma.sticker.findMany();
+
+  if (stickers.length <= 0) {
+    return <StickerModal />;
+  }
 
   return <div className="pt-20 px-4"></div>;
 }
