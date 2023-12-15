f0e045b96a8dbec1c08e656e33aee125d7993dca
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-12-15 23:31:29 +0800

    Update Resident page.

--- app/(platform)/(dashboard)/residents/[residentId]/page.tsx
+++ app/(platform)/(dashboard)/residents/[residentId]/page.tsx
@@ -1,22 +1,29 @@
 import prisma from "@/prisma/client";
 import { ResidentForm } from "./_components/resident-form";
 import { Resident } from "@prisma/client";
+import { ResidentWithOptions } from "@/types";
+import { PurchaseStickerModal } from "@/components/modals/purchase-sticker-modal";
 
 export default async function ResidentPage({
   params,
 }: {
   params: { residentId: string };
 }) {
-  const resident: Resident | null = await prisma.resident.findFirst({
-    where: {
-      id: params.residentId,
-    },
-    include: {
-      stickers: true,
-    },
-  });
+  const resident: ResidentWithOptions | null = await prisma.resident.findUnique(
+    {
+      where: {
+        id: params.residentId,
+      },
+      include: {
+        stickers: true,
+        dues: true,
+      },
+    }
+  );
+
   return (
     <div className="flex flex-col pt-20 px-5">
+      <PurchaseStickerModal />
       <ResidentForm resident={resident} />
     </div>
   );
