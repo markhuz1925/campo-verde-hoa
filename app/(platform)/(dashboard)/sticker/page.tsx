8d9926394032b5e6ca7c530c804568c25ee8e34a
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-12-15 18:14:30 +0800

    Update Sticker page.

--- app/(platform)/(dashboard)/sticker/page.tsx
+++ app/(platform)/(dashboard)/sticker/page.tsx
@@ -1,12 +1,22 @@
 import { StickerModal } from "@/components/modals/sticker-modal";
+import { PageHeading } from "@/components/page-heading";
 import prisma from "@/prisma/client";
 
 export default async function StickerPage() {
-  const stickers = await prisma.stickerPrice.findMany();
+  const stickerPrices = await prisma.stickerPrice.findMany();
 
-  if (stickers.length <= 0) {
+  if (stickerPrices.length <= 0) {
     return <StickerModal />;
   }
 
-  return <div className="pt-20 px-4"></div>;
+  return (
+    <div className="pt-20 px-4">
+      <PageHeading title="Stickers" description="Manage your stickers" />
+      {stickerPrices.map((sticker) => (
+        <div key={sticker.id}>
+          <p>{sticker.name === "ho" ? "Homeowner" : sticker.name}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
