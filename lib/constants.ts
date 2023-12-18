cb248c5f79ef668f4638db7c1f736dede9024ebd
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-12-19 00:08:09 +0800

    update constants.

--- lib/constants.ts
+++ lib/constants.ts
@@ -12,7 +12,7 @@ export const openSans = Open_Sans({
 });
 
 export const formSchema = z.object({
-  name: z.string().min(2, "Last name must be at least 2 characters"),
+  name: z.string().min(2, "Name must be at least 2 characters"),
   block: z.string().min(1, "Block number is required"),
   lot: z.string().min(1, "Lot number is required"),
   phase: z.string().min(1, "Please select a Phase"),
@@ -24,4 +24,18 @@ export const formSchema = z.object({
     .nullable(),
   isAdmin: z.boolean().optional(),
   role: z.string().min(1, "Please select a role"),
+});
+
+export const purchaseStickerFormSchema = z.object({
+  stickerNumber: z.string(),
+  stickerColor: z.string(),
+  role: z.enum(["homeowner", "tenant", "visitor", "special"]),
+  amount: z.coerce.number(),
+  name: z.string().min(3, "Name must be at least 3 characters"),
+  plate: z.string(),
+  vehicleType: z.string(),
+  vehicleColor: z.string(),
+  driverLicense: z.string(),
+  residentId: z.string(),
+  quantity: z.coerce.number(),
 });
