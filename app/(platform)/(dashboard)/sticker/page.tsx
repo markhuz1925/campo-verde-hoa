3089b1a5a1d08de12bd2a9f6807053838498dd7a
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-12-19 22:19:52 +0800

    Refactor.

--- app/(platform)/(dashboard)/sticker/page.tsx
+++ app/(platform)/(dashboard)/sticker/page.tsx
@@ -5,6 +5,8 @@ import { StickerClient } from "./_components/sticker-client";
 import { StickerColumn } from "./_components/columns";
 import { format } from "date-fns";
 import { ResidentWithOptions } from "@/types";
+import { StickersHeading } from "./_components/stickersHeading";
+import { VisitorDeliveryModal } from "@/components/modals/visitor-delivery-modal";
 
 export default async function StickerPage() {
   const stickerPrices = await prisma.stickerPrice.findMany();
@@ -26,14 +28,16 @@ export default async function StickerPage() {
     stickerDate: format(new Date(sticker.stickerDate), "MMM dd, yyyy"),
     block: sticker.resident.block,
     lot: sticker.resident.lot,
+    phase: sticker.resident.phase,
   }));
 
   if (stickerPrices.length <= 0) redirect("/settings");
 
   return (
     <div className="pt-20 px-4">
-      <PageHeading title="Stickers" description="Manage your stickers" />
-      <div className="flex flex-col gap-5">
+      <StickersHeading />
+      <VisitorDeliveryModal sticker={stickerPrices} />
+      <div className="flex flex-col gap-5 py-4">
         <StickerClient data={formattedStickers} />
       </div>
     </div>
