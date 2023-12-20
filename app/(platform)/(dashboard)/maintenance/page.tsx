7cbec0d138cab512070687c89e6c6105ed3c3b7b
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-12-21 05:57:12 +0800

    Create Maintenance page.

---
+++ app/(platform)/(dashboard)/maintenance/page.tsx
@@ -0,0 +1,12 @@
+import { PageHeading } from "@/components/page-heading";
+
+export default function MaintenancePage() {
+  return (
+    <div className="pt-20 px-5">
+      <PageHeading
+        title="Maintenance"
+        description="Manage your maintenance works."
+      />
+    </div>
+  );
+}
