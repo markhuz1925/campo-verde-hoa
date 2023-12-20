814c6c4f3af7d3fe35d9a23d19767991af3f549a
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-12-21 05:56:27 +0800

    Create Announcements page.

---
+++ app/(platform)/(dashboard)/announcements/page.tsx
@@ -0,0 +1,12 @@
+import { PageHeading } from "@/components/page-heading";
+
+export default function AnnouncementsPage() {
+  return (
+    <div className="pt-20 px-5">
+      <PageHeading
+        title="Announcements"
+        description="Manage your community announcements."
+      />
+    </div>
+  );
+}
