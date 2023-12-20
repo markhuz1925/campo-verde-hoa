08d0d210a006cd831f3d6de3482f8c5a9404ffd6
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-12-21 05:56:49 +0800

    Create Events page.

---
+++ app/(platform)/(dashboard)/events/page.tsx
@@ -0,0 +1,9 @@
+import { PageHeading } from "@/components/page-heading";
+
+export default function EventsPage() {
+  return (
+    <div className="pt-20 px-5">
+      <PageHeading title="Events" description="Manage your community events." />
+    </div>
+  );
+}
