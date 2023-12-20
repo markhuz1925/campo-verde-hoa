4f6c95f59419e412acc2f037c1949a42128314c6
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-12-21 05:56:59 +0800

    Create Financials page.

---
+++ app/(platform)/(dashboard)/financials/page.tsx
@@ -0,0 +1,9 @@
+import { PageHeading } from "@/components/page-heading";
+
+export default function FinancialsPage() {
+  return (
+    <div className="pt-20 px-5">
+      <PageHeading title="Financials" description="Manage your financials" />
+    </div>
+  );
+}
