1eaed427be5e08efade4ab77819649acd180b438
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-12-21 05:56:36 +0800

    Create Dues page.

---
+++ app/(platform)/(dashboard)/dues/page.tsx
@@ -0,0 +1,12 @@
+import { PageHeading } from "@/components/page-heading";
+
+export default function DuesPage() {
+  return (
+    <div className="pt-20 px-5">
+      <PageHeading
+        title="Dues"
+        description="Manage your Residents' monthly HOA dues"
+      />
+    </div>
+  );
+}
