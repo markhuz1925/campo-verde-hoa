84bf53ea5103968688f7117eb455c5b648392111
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2024-01-01 15:07:13 +0800

    Fix

--- types.ts
+++ types.ts
@@ -3,5 +3,4 @@ import { Dues, Resident, Sticker, Transaction } from "@prisma/client";
 export type ResidentWithOptions = Resident & {
   stickers: Sticker[];
   dues: Dues[];
-  transaction: Transaction[];
 };
