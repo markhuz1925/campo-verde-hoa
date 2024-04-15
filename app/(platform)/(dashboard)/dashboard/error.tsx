debf52ab219fba5ff5fdcff98a583342e026c31d
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2024-04-15 14:02:35 +0800

    Add padding top and bottom.

--- app/(platform)/(dashboard)/dashboard/error.tsx
+++ app/(platform)/(dashboard)/dashboard/error.tsx
@@ -1,24 +1,23 @@
 "use client";
 
-import {useEffect} from "react";
-import {Button} from "@/components/ui/button";
+import { useEffect } from "react";
+import { Button } from "@/components/ui/button";
 
-export default function Error({error, reset,}: {
-  error: Error & { digest?: string }
-  reset: () => void
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
 }) {
   useEffect(() => {
-    console.error(error)
-  }, [error])
+    console.error(error);
+  }, [error]);
 
   return (
-    <div className="flex items-center justify-center gap-5">
+    <div className="flex items-center justify-center gap-5 pt-20 pb-20">
       <h2 className="text-2xl font-bold">Something went wrong!</h2>
-      <Button
-        onClick={() => reset()}
-      >
-        Try again
-      </Button>
+      <Button onClick={() => reset()}>Try again</Button>
     </div>
-  )
+  );
 }
